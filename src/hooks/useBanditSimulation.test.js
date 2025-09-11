2e5e711a77d778edaad459cb22fb5a514bfe470f
mekkev1 <mekelburgkevin@gmail.com>
2025-09-11 19:33:33 +0200

    CHORE: Ordner Refactoring

---
+++ src/hooks/useBanditSimulation.test.js
@@ -0,0 +1,10 @@
+import * as UseBanditSimulation from './useBanditSimulation.js';
+import { describe } from 'vitest';
+import { test, expect } from 'vitest';
+
+describe('useBanditSimulation.js', () => {
+
+    test('Vitest läuft', () => {
+        expect(true).toBe(true);
+    });
+});
---
+++ src/utils/math.test.js
@@ -0,0 +1,10 @@
+import * as Math from './math.js';
+import { describe } from 'vitest';
+import { test, expect } from 'vitest';
+
+describe('math.js', () => {
+
+    test('Vitest läuft', () => {
+        expect(true).toBe(true);
+    });
+});
