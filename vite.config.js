957b2aa674fd19af9f90025a898e4ff13528e455
mekkev1 <mekelburgkevin@gmail.com>
2025-09-15 22:14:29 +0200

    CHORE: Fix Deployment

--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,14 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
+const repoName = '23DWF1_multi-bandits_G5' // dein GitHub-Repo-Name
+// eslint-disable-next-line no-undef
+const base = process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/'
+
 export default defineConfig({
-    plugins: [
-        tailwindcss(),
-        react()
-    ],
+    plugins: [tailwindcss(), react()],
     resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
+        alias: { "@": path.resolve(__dirname, "./src") }
     },
+    base
 })
