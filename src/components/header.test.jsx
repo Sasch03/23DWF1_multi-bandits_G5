1e0d4ff87f605a20fcd8f78e031ebe6624530f5d
mekkev1 <mekelburgkevin@gmail.com>
2025-09-26 17:35:18 +0200

    TEST: Fix Test

--- src/components/header.test.jsx
+++ src/components/header.test.jsx
@@ -1,7 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import Header from "./Header";
+import Header from "./header.jsx";
 
 describe("Header Component", () => {
     it("renders the main heading", () => {
