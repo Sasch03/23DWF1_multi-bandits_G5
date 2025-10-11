3948dd7d9bf0a0c2fe758bb2d02b0c3041822c11
aleksaleo <aleksandr.leongard@mail.de>
2025-10-11 19:45:20 +0200

    DOC: Changed documentation of AlgorithmTyp.js

--- src/logic/enumeration/AlgorithmTyp.js
+++ src/logic/enumeration/AlgorithmTyp.js
@@ -1,6 +1,6 @@
 /**
  * Represents an enumeration with the choosable algorithm typs.
- * @type {Readonly<{GREEDY: string, EPSILON_GREEDY: string}>}
+ * @type {Readonly<{GREEDY: string, EPSILON_GREEDY: string, GRADIENT_BANDIT: string, UPPER_CONFIDENCE_BOUND: string}>}
  */
 export const AlgorithmTyp = Object.freeze({
     GREEDY: "Greedy",
