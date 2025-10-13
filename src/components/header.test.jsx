74c4d36ba2d3d01529b988aab27d8c4be494f0e2
mekkev1 <mekelburgkevin@gmail.com>
2025-10-13 16:18:07 +0200

    TEST: fix header import

--- src/components/header.test.jsx
+++ src/components/header.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import Header from "./Header";
+import Header from "@/components/header.jsx";
 import { describe, it, expect } from "vitest";
 
 describe("Header Component", () => {
