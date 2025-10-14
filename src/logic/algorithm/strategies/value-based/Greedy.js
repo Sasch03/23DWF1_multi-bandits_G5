873876021c8dd230a3a787c3cc5951ca0048aced
DariaL-git <daria.ludwig@noz-digital.de>
2025-10-14 22:35:34 +0200

    DOC: Greedy.js

--- src/logic/algorithm/strategies/value-based/Greedy.js
+++ src/logic/algorithm/strategies/value-based/Greedy.js
@@ -4,6 +4,7 @@ import ValueBasedAlgorithm from "@/logic/algorithm/ValueBasedAlgorithm.js";
 
 /**
  * Greedy algorithm (special case of ε-Greedy with ε = 0).
+ *
  * Always chooses the arm with the maximum expected value.
  * Exists as a separate class for clarity and clean hierarchy.
  */
